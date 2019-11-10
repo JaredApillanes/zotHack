835400bb9c5a36dee7311b1d77d356e7a2678bd6
Nathan Tang <nathanwt@uci.edu>
2019-11-10 12:44:32 -0800

    added redirect

--- src/app/views/Home/Home.js
+++ src/app/views/Home/Home.js
@@ -1,21 +1,28 @@
-import logo from "./league-of-legends-logo.png";
+import logo from "./titleLogo.png";
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import './Home.css';
 
-function Home(e) {
+
+function Home(props) {
+    async function handleSubmit(e) {
+        props.history.push('/Play/');
+    }
     return (
         <div className="App-header">
             <header>
             <img src={logo} className="App-logo" alt="logo" />
             </header>
+        <Form className = "start-game" onSubmit={handleSubmit}>
           <Form.Group controlId="name">
               <Form.Text>
-                  Enter Username:
+                  <p>Enter Username:</p>
               </Form.Text>
-                <p></p> <Form.Control placeholder="Enter your name"></Form.Control>
+              <Form.Control placeholder="Enter your name"></Form.Control>
           </Form.Group>
-            <Button variant = 'secondary'>Test</Button>
+            <Button type='submit'>Play</Button>
+        </Form>
         </div>
     )
 }
