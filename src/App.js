1f80c69eda3b86a01d792d874bfd6e52802877e6
Jared Apillanes <jared.apillanes@gmail.com>
2019-11-10 10:54:06 -0800

    BoilerPlate Code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch, Router} from 'react-router-dom';
 import history from './history.js';
 import './App.css';
 import Home from './app/views/Home/Home'
+import Play from './app/views/Play/Play'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Router history={history}>
                 <Switch>
                     <Route exact path='/' component={Home}/>
+                    {/*<Route exact path='/Play' component={Play}/>*/}
                 </Switch>
             </Router>
         </div>
