de99b28451a9eabfdd772a010469ff66b94e2b44
Nathan Tang <nathanwt@uci.edu>
2019-11-10 14:30:05 -0800

    Merge branch 'frontend' of C:\Users\Tangy\Desktop\zotHacks\zotHack with conflicts.

--- src/app/views/Home/Home.js
+++ src/app/views/Home/Home.js
@@ -6,23 +6,25 @@ import './Home.css';
 
 
 function Home(props) {
+
     async function handleSubmit(e) {
         props.history.push('/Play/');
     }
+
     return (
         <div className="App-header">
             <header>
-            <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo} className="App-logo" alt="logo"/>
             </header>
-        <Form className = "start-game" onSubmit={handleSubmit}>
-          <Form.Group controlId="name">
-              <Form.Text>
-                  <p>Enter Username:</p>
-              </Form.Text>
-              <Form.Control placeholder="Enter your name"></Form.Control>
-          </Form.Group>
-            <Button type='submit'>Play</Button>
-        </Form>
+            <Form className="start-game" onSubmit={handleSubmit}>
+                <Form.Group controlId="name">
+                    <Form.Text>
+                        <p>Enter Username:</p>
+                    </Form.Text>
+                    <Form.Control placeholder="Enter your name"></Form.Control>
+                </Form.Group>
+                <Button type='submit'>Play</Button>
+            </Form>
         </div>
     )
 }
